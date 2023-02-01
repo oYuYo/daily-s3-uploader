fb85204d702958da7c820da46a62a835e3153ac2
oYuYo <year0120@gmail.com>
2023-02-02 01:17:40 +0900

    up

---
+++ get_photo.rb
@@ -0,0 +1,50 @@
+#! /usr/bin/ruby
+
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+def save_photo(rnd, i, result)
+
+  #reference of size -> https://www.flickr.com/services/api/misc.urls.html
+  server_id = result[:photos][:photo][rnd][:server]
+  id = result[:photos][:photo][rnd][:id]
+  secret = result[:photos][:photo][rnd][:secret]
+  size_suff = 'c'
+
+  url = URI.parse("https://live.staticflickr.com/#{server_id}/#{id}_#{secret}_#{size_suff}.jpg")
+  file = "./test#{i}.jpg"
+
+  URI.open(file, 'wb') do |pass|
+    URI.open(url) do |recieve|
+      pass.write(recieve.read)
+    end
+  end
+end
+
+api_key = ENV["API_KEY"]
+user_id = ENV["USER_ID"]
+uri = URI.parse("https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=#{api_key}&user_id=#{user_id}&format=json&nojsoncallback=1")
+begin
+
+  res = Net::HTTP.get_response(uri)
+  case res
+  when Net::HTTPSuccess
+    result = JSON.parse(res.body, {symbolize_names: true})
+    max_size = result[:photos][:photo].size
+
+    2.times do |i|
+      random = Random.new
+      rnd = random.rand(0...max_size)
+      save_photo(rnd, i+1, result)
+      result[:photos][:photo].delete_at(rnd)
+    end
+  else
+    p response.header
+  end
+
+rescue JSON::ParserError
+  puts "JSON::ParserError"
+rescue => e
+  puts e.message
+end
