e13afb7d1047af4efa3b2253d6bbc235b73b4bf5
oYuYo <year0120@gmail.com>
2023-11-30 14:38:01 +0900

    up

--- convert-mp4.sh
+++ convert-mp4.sh
@@ -2,4 +2,3 @@
 #メジャーなコーデックとしてlibx264(baseline)を指定している
 #ピクセルフォーマットはフルレンジyuvj420を指定
 ffmpeg -loop 1 -r 30 -t 1 -i ./pics1.jpg -codec:v libx264 -profile:v baseline -pix_fmt yuvj420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ./pics1.mp4 -y
-ffmpeg -loop 1 -r 30 -t 1 -i ./pics2.jpg -codec:v libx264 -profile:v baseline -pix_fmt yuvj420p -vf "scale=trunc(iw/2)*2:trunc(ih/2)*2" ./pics2.mp4 -y
